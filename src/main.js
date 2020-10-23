71e943e5d12c39ae49b582dd0cbf99a486088234
atom-11 <pinalelo@ymobile.ne.jp>
2020-10-23 10:06:04 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBllMYRnD0tIum9xdSzsHdGt_OK5XRLzhQ",
+  authDomain: "vue-todo-b9c9e.firebaseapp.com",
+  databaseURL: "https://vue-todo-b9c9e.firebaseio.com",
+  projectId: "vue-todo-b9c9e",
+  storageBucket: "vue-todo-b9c9e.appspot.com",
+  messagingSenderId: "663003617878",
+  appId: "1:663003617878:web:4d0a3abaa6b5d7570af9f7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   render: h => h(App),
